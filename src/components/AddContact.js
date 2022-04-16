2e285fa8ab0c250b632836b42da0b25beb0a74a4
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-04-16 06:12:06 -0400

    input form

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function AddContact() {
-  return <h1>Contact.js</h1>;
+class AddContact extends Component {
+  render() {
+    return (
+      <div className="ui main">
+        <h2>Add Contact</h2>
+        <form className="ui form">
+          <div className="field">
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Name"></input>
+          </div>
+          <div className="field">
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Name"></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default AddContact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Header() {
-  return <h1>Header.js</h1>;
-}
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>Contact Manager</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
